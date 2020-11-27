2a5dd0b321ebe5938b92b8958204bb3f2e353b04
BrianTM <bryan98tm@gmail.com>
2020-11-27 12:32:56 -0500

    Se agrego la interfazde listado de cursos y las logicas de paginacion

--- src/components/Cursos/PaginadorCurso.js
+++ src/components/Cursos/PaginadorCurso.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { paginacionCurso } from '../../actions/CursoAction';
+import { Hidden, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from '@material-ui/core';
 
 const PaginadorCurso = () => {
     const [paginadorRequest, setPaginadorRequest] = useState({
@@ -21,17 +22,71 @@ const PaginadorCurso = () => {
             cantidadElementos: paginadorRequest.cantidadElementos
         }
 
-        paginacionCurso(objetoPaginadorRequest)
-                .then(response => {
-                    console.log('response Paginador', response);
-                })
-                .catch(error => console.log(error));
+        const obtenerListaCurso = async() => {
+            const response = await paginacionCurso(objetoPaginadorRequest);
+            setPaginadorResponse(response.data);
+        }
+
+        obtenerListaCurso();
     }, [paginadorRequest])
 
+    const cambiarPagina = (event, nuevaPagina) => {
+        setPaginadorRequest((anterior) => ({
+            ...anterior,
+            numeroPagina: parseInt(nuevaPagina)
+        }));
+    }
+
+    const cambiarCantidadRecords = (event) => {
+        setPaginadorRequest((anterior) => ({
+            ...anterior,
+            cantidadElementos: parseInt(event.target.value),
+            numeroPagina: 0
+        }));
+    }
+
     return (    
-        <div>
-            
-        </div>
+       <React.Fragment>
+           <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="left">ID</TableCell>
+                            <TableCell align="left">Cursos</TableCell>
+                            <Hidden mdDown>
+                                <TableCell align="left">Descripcion</TableCell>
+                                <TableCell align="left">FechaPublicacion</TableCell>
+                                <TableCell align="left">Precio Normal</TableCell>
+                                <TableCell align="left">Precio Oferta</TableCell>
+                            </Hidden>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {paginadorResponse.listaRecords.map((curso, i = 1) => (
+                            <TableRow key={curso.titulo}>
+                                <TableCell align="left">{i++}</TableCell>
+                                <TableCell align="left">{curso.Titulo}</TableCell>
+                                <Hidden mdDown>
+                                    <TableCell align="left">{curso.Descripcion}</TableCell>
+                                    <TableCell align="left">{(new Date(curso.FechaPublicacion)).toLocaleString()}</TableCell>
+                                    <TableCell align="left">{curso.PrecioActual}</TableCell>
+                                    <TableCell align="left">{curso.PrecioPromocion}</TableCell>
+                                </Hidden>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+           </TableContainer>
+           <TablePagination 
+                rowsPerPageOptions={[5, 10, 25]}//Combo box para decir caunta data puede pintar en pantalla
+                count={paginadorResponse.totalRecords}//Cantidad de elemtos que tiene en total la tabla que quiero paginar
+                rowsPerPage={paginadorRequest.cantidadElementos}//Indica cuantas filas quiero imprimir dentro de la pagina
+                page={paginadorRequest.numeroPagina}//Indica que en pagina estoy
+                onChangePage={cambiarPagina}
+                onChangeRowsPerPage={cambiarCantidadRecords}//Este evento se disprara cuando el usuario seleccione un item del combo box
+                labelRowsPerPage="Cursos por Pagina"
+            />
+       </React.Fragment>
     );
 };
 
